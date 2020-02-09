513788ff967505952e794087d82aab31a74e329e
Ralzar81 <57372363+Ralzar81@users.noreply.github.com>
2020-02-09 23:03:53 +0100

    Changed to using Update

--- Filling Food/FillingFood.cs
+++ Filling Food/FillingFood.cs
@@ -29,59 +29,64 @@ namespace FillingFood
             mod = initParams.Mod;
             var go = new GameObject(mod.Title);
             go.AddComponent<FillingFood>();
-            EntityEffectBroker.OnNewMagicRound += FoodEffects_OnNewMagicRound;
 
         }
 
         void Awake()
         {
             mod.IsReady = true;
-            Debug.Log("Filling Food ready");
+            Debug.Log("[FillingFood Food] Mod Is Ready");
         }
 
         static PlayerEntity playerEntity = GameManager.Instance.PlayerEntity;
-        static private bool foodTxt = true;
-        static private bool hungerTxt = false;
+        static private bool hungry = true;
         static private int foodCount = 0;
+
+
+        void Update()
+        {
+            uint gameMinutes = DaggerfallUnity.Instance.WorldTime.DaggerfallDateTime.ToClassicDaggerfallTime();
+            uint ateTime = GameManager.Instance.PlayerEntity.LastTimePlayerAteOrDrankAtTavern;
+            uint hunger = (gameMinutes - ateTime);
+            if (hunger <= 240 && hungry)
+            {
+                hungry = false;
+                EntityEffectBroker.OnNewMagicRound += FoodEffects_OnNewMagicRound;
+                DaggerfallUI.AddHUDText("You feel invigorated by the meal.");
+                Debug.Log("[FillingFood Food] Registering OnNewMagicRound");
+            }
+        }
+
+
 
         private static void FoodEffects_OnNewMagicRound()
         {
+            Debug.Log("[FillingFood Food] Round Start");
+
             uint gameMinutes = DaggerfallUnity.Instance.WorldTime.DaggerfallDateTime.ToClassicDaggerfallTime();
             uint ateTime = GameManager.Instance.PlayerEntity.LastTimePlayerAteOrDrankAtTavern;
             uint hunger = (gameMinutes - ateTime);
 
             if ( hunger <= 240)
             {
-                if (foodTxt)
-                {
-                    DaggerfallUI.AddHUDText("You feel invigorated by the meal.");
-                    foodTxt = false;
-                    hungerTxt = true;
-                }
-
                 foodCount += (200 - (int)hunger);
-
                 Debug.Log(foodCount.ToString());
-
                 if (foodCount >= 500)
                 {
                     playerEntity.IncreaseFatigue(1, true);
                     foodCount = 0;
-                    Debug.Log("FillingFood +1 Fatigue");
+                    Debug.Log("[FillingFood Food] +1 Fatigue");
                 }
             }
             else
             {
-                foodTxt = true;
-                if (hunger >= 240 && hungerTxt)
-                {
-                    DaggerfallUI.AddHUDText("Your stomach rumbles...");
-                    hungerTxt = false;
-                }
-                Debug.Log("FillingFood Hungry");
+                Debug.Log("[FillingFood Food] Hungry");
+                hungry = true;
+                DaggerfallUI.AddHUDText("Your stomach rumbles...");
+                EntityEffectBroker.OnNewMagicRound -= FoodEffects_OnNewMagicRound;
+                Debug.Log("[FillingFood Food] De-registering from OnNewMagicRound");
             }
+            Debug.Log("[FillingFood Food] Round End");
         }
-
-
     }
 }
